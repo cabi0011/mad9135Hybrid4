478e51dbcd20df46cd9995c42aeccba55803cce7
Gina Cabildo <cabi0011@algonquinlive.com>
2024-10-31 21:09:39 -0400

    Assignment completed

--- bin/hybrid4.dart
+++ bin/hybrid4.dart
@@ -1,5 +1,27 @@
 import 'package:hybrid4/hybrid4.dart' as hybrid4;
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-void main(List<String> arguments) {
-  print('Hello world: ${hybrid4.calculate()}!');
+void main() async {
+  const url = 'https://random-data-api.com/api/users/random_user?size=10';
+
+  try {
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      List<dynamic> users = jsonDecode(response.body);
+
+      for (var user in users) {
+        print('${user['uid']}: ${user['first_name']} ${user['last_name']}');
+      }
+    } else {
+      print('Failed to load users');
+    }
+  } catch (e) {
+    print('Error: $e');
+  }
 }
+
+// void main(List<String> arguments) {
+//   print('Hello world: ${hybrid4.calculate()}!');
+// }
